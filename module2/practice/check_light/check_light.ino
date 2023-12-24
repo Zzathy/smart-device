533757a7be247d07f3742dc2ecc6a95ca233c86e
Izza Ihsan Fathony <izzafathony27@gmail.com>
2023-12-24 23:24:15 +0700

    feat: :sparkles: finish module2

---
+++ module2/practice/check_light/check_light.ino
@@ -0,0 +1,22 @@
+// const int ldrPin = 21;
+// const int buzzerPin = 25;
+
+// int switchState = 0;
+
+// void setup() {
+//   Serial.begin(115200);
+//   pinMode(ldrPin, INPUT);
+//   pinMode(buzzerPin, OUTPUT);
+// }
+
+// void loop() {
+//   switchState = digitalRead(ldrPin);
+
+//   if (switchState == LOW) {
+//     digitalWrite(buzzerPin, LOW);
+//     Serial.println("Output : Bright");
+//   } else {
+//     digitalWrite(buzzerPin, HIGH);
+//     Serial.println("Output : Dark");
+//   }
+// }
